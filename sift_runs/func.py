445a61399f386c03ec284d7f80fe94bed944c531
devmgardner <devm.gardner@gmail.com>
2021-12-20 15:50:03 -0500

    finds the 5 button on windows calculator and clicks

--- sift_runs/func.py
+++ sift_runs/func.py
@@ -5,8 +5,7 @@
 #https://pyautogui.readthedocs.io/en/latest/
 import pyautogui
 import os, sys, time, pandas as pd, numpy as np
-import matplotlib.pyplot as plt
-#matplotlib inline
+#import matplotlib.pyplot as plt
 #from skimage.io import imread, imshow
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -14,9 +13,14 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 #
 test_image1 = os.path.join(currentdir, 'test_image_1.PNG')
+button5 = os.path.join(currentdir, 'button5.PNG')
 #test_image = open(test_image)
 #image = imread(test_image1, as_gray=True)
 #imshow(image)
 #image.shape, image
-location = pyautogui.locateCenterOnScreen(test_image1)
-print(f'X = {location[0]} // Y = {location[1]}')
+location = pyautogui.locateCenterOnScreen(button5, confidence=0.5)
+if not location == None:
+    print(f'X = {location[0]} // Y = {location[1]}')
+    pyautogui.leftClick(button5)
+else:
+    print(f'Image not found. Ending.')
