fcc80101f6c1262a3086c9020fabd613de4b4f5c
devmgardner <devm.gardner@gmail.com>
2021-12-20 11:39:21 -0500

    tinkering with image recognition

--- sift_runs/func.py
+++ sift_runs/func.py
@@ -1,5 +1,8 @@
+#https://www.analyticsvidhya.com/blog/2019/08/3-techniques-extract-features-from-image-data-machine-learning-python/
 import pyautogui
 import os, sys, time, pandas as pd, numpy as np
+import matplotlib.pyplot as plt
+#matplotlib inline
 from skimage.io import imread, imshow
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -10,3 +13,4 @@ test_image = os.path.join(currentdir, 'test_image_1.PNG')
 #test_image = open(test_image)
 image = imread(test_image, as_gray=True)
 imshow(image)
+image.shape, image
