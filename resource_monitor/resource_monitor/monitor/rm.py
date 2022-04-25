fea26fa476d356c2e6463d4bcef8bdb1b16adf83
devmgardner <devm.gardner@gmail.com>
2022-04-25 19:53:05 +0000

    updates to resource monitor, now includes disk usage and disk io stats

--- resource_monitor/resource_monitor/monitor/rm.py
+++ resource_monitor/resource_monitor/monitor/rm.py
@@ -32,7 +32,7 @@ def phys_memory():
     statement = f"Memory Usage:\n----------------\nTotal physical memory:\t{data['total']}\nTotal available memory:\t{data['available']}\nUsed memory percentage:\t{round(((pull_data[0]-pull_data[1])/pull_data[0])*100,4)}%\n----------------"
     return statement
 def disk_stats():
-    part_data = ps.disk_partitions()
+    part_data = ps.disk_partitions(all=False)
     io_data = ps.disk_io_counters(perdisk=True)
     data = {}
     def convert_to_bytes(inp):
@@ -49,7 +49,7 @@ def disk_stats():
         data[i[0]]['total'] = f'{convert_to_bytes(du[0])} - {du[0]}'
         data[i[0]]['used'] = f'{convert_to_bytes(du[1])} - {du[1]}'
         data[i[0]]['free'] = f'{convert_to_bytes(du[2])} - {du[2]}'
-        data[i[0]]['percents'] = f"{round(data[i[0]]['used']/data[i[0]]['total'],4)}% used, {round(data[i[0]]['free']/data[i[0]]['total'],4)}% free"
+        data[i[0]]['percents'] = f"{round(du[1]/du[0],4)*100}% used, {round(du[2]/du[0],4)*100}% free"
         data[i[0]]['mountpoint'] = i[1]
         data[i[0]]['type'] = i[2]
     statements = []
@@ -58,10 +58,10 @@ def disk_stats():
     statements.append(f'----------------')
     for name in data.keys():
         statements.append(f'++++++++++++++++')
-        statements.append(f'Name is:\t{name}')
-        statements.append(f"Mount point is:\t{data[name]['mountpoint']}")
+        statements.append(f'Name is:\t\t{name}')
+        statements.append(f"Mount point is:\t\t{data[name]['mountpoint']}")
         statements.append(f"Filesystem type is:\t{data[name]['type']}")
-        statements.append(f"Total space is:\t{data[name]['total']}")
+        statements.append(f"Total space is:\t\t{data[name]['total']}")
         statements.append(f"Total space used is:\t{data[name]['used']}")
         statements.append(f"Total space free is:\t{data[name]['free']}")
         statements.append(data[name]['percents'])
@@ -71,13 +71,13 @@ def disk_stats():
     statements.append(f'----------------')
     for i in io_data.keys():
         statements.append(f'++++++++++++++++')
-        statements.append(f'Name is:\t{i}')
-        statements.append(f"Read count is:\t{io_data[i][0]}")
-        statements.append(f"Write count is:\t{io_data[i][1]}")
-        statements.append(f"Read bytes are:\t{convert_to_bytes(io_data[i][2])}")
+        statements.append(f'Name is:\t\t{i}')
+        statements.append(f"Read count is:\t\t{io_data[i][0]}")
+        statements.append(f"Write count is:\t\t{io_data[i][1]}")
+        statements.append(f"Read bytes are:\t\t{convert_to_bytes(io_data[i][2])}")
         statements.append(f"Write bytes are:\t{convert_to_bytes(io_data[i][3])}")
-        statements.append(f"Read time is:\t{io_data[i][4]*1000}")
-        statements.append(f"Write time is:\t{io_data[i][5]*1000}")
+        statements.append(f"Read time is:\t\t{io_data[i][4]/1000}")
+        statements.append(f"Write time is:\t\t{io_data[i][5]/1000}")
         statements.append(f'++++++++++++++++')
     statements.append(f'----------------')
     return '\n'.join(statements)
