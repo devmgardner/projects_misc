1199d8fe2022c1aab9c33522ac230703ff1bce29
devmgardner <devm.gardner@gmail.com>
2022-04-25 10:55:30 -0400

    updated to add average load function

--- resource_monitor/rm.py
+++ resource_monitor/rm.py
@@ -1,9 +1,21 @@
+from xml.dom import pulldom
 import psutil as ps
+#returns active load percentage in categories
 def cpu():
     pull_data = ps.cpu_times_percent(interval=0.5)
     data = {}
     data['user'], data['system'], data['idle'], data['iowait'] = pull_data[0], pull_data[2], pull_data[3], pull_data[4]
     statement = f"User:\t{data['user']}\nSystem:\t{data['system']}\nIdle:\t{data['idle']}\nIOWait:\t{data['iowait']}"
     return statement
-while True:
-    print(cpu())
+#returns average load over 1, 5, and 15 minutes
+def avgload():
+    pull_data = [x / ps.cpu_count() * 100 for x in ps.getloadavg()]
+    data = {}
+    data['1min'], data['5min'], data['15min'] = pull_data[0], pull_data[1], pull_data[2]
+    statement = f"1 minute load average:\t{data['1min']}\n5 minute load average:\t{data['5min']}\n15 minute load average:\t{data['15min']}"
+    return statement
+print(cpu)
+print(avgload)
+
+#while True:
+#    print(cpu())
