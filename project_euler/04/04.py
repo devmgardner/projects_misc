a4185cd177a8ad170d44cd336473158b2a5d23d3
devmgardner <devm.gardner@gmail.com>
2022-05-16 13:58:09 -0400

    problem 04 complete

--- project_euler/04/04.py
+++ project_euler/04/04.py
@@ -16,3 +16,29 @@ Find the largest palindrome made from the product of two 3-digit numbers."""
 #content = re.sub(f'\<p[^\>]*\>','',content)
 #content = re.sub(f'\<\/p\>','',content)
 #print(content)
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+fname = os.path.join(currentdir, f'04-log.txt')
+fhand = open(fname, 'w')
+fhand.close()
+fhand = open(fname, 'r+')
+#
+from itertools import product
+from operator import itemgetter
+def is_palindrome(inp):
+    if ''.join(reversed(list(str(inp)))) == str(inp):
+        return True
+    else:
+        return False
+
+pallist = []
+
+lista = listb = [i for i in range(1000) if i > 100 and not str(i).endswith('0')]
+for i1,i2 in product(lista, listb):
+    if is_palindrome(i1*i2) == True:
+        pallist.append([i1,i2,i1*i2])
+
+for i in sorted(pallist, key=itemgetter(2)):
+    fhand.write(f'{i}\n')
