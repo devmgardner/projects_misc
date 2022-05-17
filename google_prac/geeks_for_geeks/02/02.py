08aa09585bf982f22152e29f3eab260691af773d
devmgardner <devm.gardner@gmail.com>
2022-05-17 15:11:33 -0400

    rearranging folder

---
+++ google_prac/geeks_for_geeks/02/02.py
@@ -0,0 +1,5 @@
+#https://practice.geeksforgeeks.org/explore?page=1&company[]=Google&sortBy=submissions
+#https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1/?page=1&company[]=Google&sortBy=submissions#
+"""Given an array of N integers arr[] where each element represents the max number of steps that can be made forward from that element. Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
+Note: Return -1 if you can't reach the end of the array."""
+
