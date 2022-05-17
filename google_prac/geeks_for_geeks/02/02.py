174e6d81f9e7c77727c2e9133adde485fe79e96f
devmgardner <devm.gardner@gmail.com>
2022-05-17 15:13:14 -0400

    starting problem 2

--- google_prac/geeks_for_geeks/02/02.py
+++ google_prac/geeks_for_geeks/02/02.py
@@ -3,3 +3,26 @@
 """Given an array of N integers arr[] where each element represents the max number of steps that can be made forward from that element. Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
 Note: Return -1 if you can't reach the end of the array."""
 
+#User function Template for python3
+class Solution:
+    def minJumps(self, arr, n):
+        elem, jumps = 0, 0
+        while not elem-1 >= len(arr):
+            try:
+                elem += arr[elem]
+            except IndexError:
+                return jumps
+            jumps += 1
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		Arr = [int(x) for x in input().split()]
+		ob = Solution()
+		ans = ob.minJumps(Arr,n)
+		print(ans)
+# } Driver Code Ends
