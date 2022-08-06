c07530158683ae5953be8f2718119600e4dcd0b2
devmgardner <devm.gardner@gmail.com>
2022-08-06 12:09:00 +0000

    set up quarry script

---
+++ minecraft/turtles/quarry.lua
@@ -0,0 +1,62 @@
+print("How many layers down would you like to go?")
+layers = read()
+for var=1,layers do
+    for var=1,7 do
+        for var=1,15 do
+            while turtle.detectDown() do
+                turtle.digDown()
+            end
+            turtle.forward()
+        end
+        while turtle.detectDown() do
+            turtle.digDown()
+        end
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnRight()
+        for var=1,15 do
+            while turtle.detectDown() do
+                turtle.digDown()
+            end
+            turtle.forward()
+        end
+        while turtle.detectDown() do
+            turtle.digDown()
+        end
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnLeft()
+    end
+    for var=1,15 do
+        while turtle.detectDown() do
+            turtle.digDown()
+        end
+        turtle.forward()
+    end
+    while turtle.detectDown() do
+        turtle.digDown()
+    end
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
+    for var=1,15 do
+        while turtle.detectDown() do
+            turtle.digDown()
+        end
+        turtle.forward()
+    end
+    while turtle.detectDown() do
+        turtle.digDown()
+    end
+    turtle.turnRight()
+    for var=1,15 do
+        turtle.forward()
+    end
+    turtle.turnRight()
+    turtle.select(1)
+    turtle.placeUp()
+    for var=2,16 do
+        turtle.select(var)
+        turtle.dropUp()
+    end
+end
