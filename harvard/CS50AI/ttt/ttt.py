4ff2e1ae4de836bf849c44d7b5a70e506ca08cdd
devmgardner <devm.gardner@gmail.com>
2022-08-09 17:27:32 +0000

    recursion error

--- harvard/CS50AI/ttt/ttt.py
+++ harvard/CS50AI/ttt/ttt.py
@@ -2,10 +2,10 @@
 # S(0) = initial State
 class ttt():
     def __init__(self):
-        self.state = [['-','-','-'],['-','-','-'],['-','-','-']]
+        self.state = [['-','X','-'],['-','O','-'],['-','X','-']]
         self.rows = self.board = []
     def Player(self,s):
-        self.board = [i for i in j for j in self.state]
+        self.board = [i for j in self.state for i in j]
         if self.board.count('X') == 0:
             return 'X'
         elif self.board.count('X') == self.board.count('O'):
@@ -13,9 +13,24 @@ class ttt():
         elif self.board.count('X') > self.board.count('O'):
             return 'O'
     def Actions(self,s):
-        pass
+        player = self.Player(s)
+        states = []
+        for ind1,i in enumerate(self.state):
+            for ind2,j in enumerate(i):
+                if j == '-':
+                    self.state[ind1][ind2] = player
+                    states.append(self.state)
+                    self.state[ind1][ind2] = '-'
+        return states
     def Result(self,s,a):
-        pass
+        result = []
+        for i in self.state:
+            result.append(i)
+        for ind1,i in enumerate(self.state):
+            for ind2,j in enumerate(i):
+                if j != '-':
+                    result[ind1][ind2] = j
+        return result
     def Terminal(self,s):
         # append existing rows to list
         for i in self.state:
@@ -45,9 +60,11 @@ class ttt():
         # if no winners but self.board is full, also return True
         elif self.board.count('-') == 0:
             return True
+        else:
+            return False
     def Utility(self,s):
         # flatten self.state into a 1d list
-        self.board = [i for i in j for j in self.state]
+        self.board = [i for j in self.state for i in j]
         # check if any potential winner for X player
         if ['X','X','X'] in self.rows:
             return 1
@@ -57,6 +74,22 @@ class ttt():
         # if no winners but self.board is full, also return True
         elif self.board.count('-') == 0:
             return 0
+    def Max_Value(self,s):
+        if self.Terminal(s):
+            return self.Utility(s)
+        v = float('-inf')
+        for action in self.Actions(s):
+            v = max(v, self.Min_Value(self.Result(s,action)))
+        return v
+    def Min_Value(self,s):
+        if self.Terminal(s):
+            return self.Utility(s)
+        v = float('inf')
+        for action in self.Actions(s):
+            v = min(v, self.Max_Value(self.Result(s,action)))
+        return v
+
 
 new = ttt()
-new.Terminal(new.state)
+print(new.Terminal(new.state))
+print(new.Result(new.state, [['O','-','-'],['-','-','-'],['-','-','-']]))
