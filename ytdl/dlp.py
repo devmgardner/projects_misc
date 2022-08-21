cd7077b19f9fef31fc64c0ee80e120e12df28517
devmgardner <devm.gardner@gmail.com>
2022-08-21 19:25:12 -0400

    shell=True

--- ytdl/dlp.py
+++ ytdl/dlp.py
@@ -6,7 +6,7 @@ sys.path.append(parentdir)
 #
 a = os.path.join('/home','ubuntu','youtube.txt')
 #
-subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail -a {a} --recode-video mp4 -o "%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s"')
+subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail -a {a} --recode-video mp4 -o "%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s"',shell=True)
 #
 with open(a,'w') as b:
     b.write('')
