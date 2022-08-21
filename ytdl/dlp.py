c2a18e1a8cf3aa43321e8f85ebf8db68563c4555
devmgardner <devm.gardner@gmail.com>
2022-08-21 19:41:05 -0400

    timestamped folders

--- ytdl/dlp.py
+++ ytdl/dlp.py
@@ -6,8 +6,10 @@ sys.path.append(parentdir)
 #
 a = [line.strip() for line in open(os.path.join('/home','ubuntu','youtube.txt')).readlines()]
 #
+now = datetime.datetime.fromtimestamp(time.time()).strftime("%m-%d-%Y_%H:%M:%S")
+#
 for b in a:
-    subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "/media/Dock1/Media/Videos/Dailies/{datetime.datetime.fromtimestamp(time.time()).strftime("%m-%d-%Y_%H:%M:%S")}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
+    subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "/media/Dock1/Media/Videos/Dailies/{now}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
 #
 with open(os.path.join('/home','ubuntu','youtube.txt'),'w') as c:
     c.write('')
