85c3e5e2bb6f371bb3a0d6c13fa3b6c43b5a39a7
devmgardner <devm.gardner@gmail.com>
2022-08-21 19:24:19 -0400

    new ytdl script using yt-dlp

---
+++ ytdl/dlp.py
@@ -0,0 +1,12 @@
+import os,sys,subprocess
+#
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+#
+a = os.path.join('/home','ubuntu','youtube.txt')
+#
+subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail -a {a} --recode-video mp4 -o "%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s"')
+#
+with open(a,'w') as b:
+    b.write('')
