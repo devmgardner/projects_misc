7d2bd7132f20bd4937de7848a709aeac2ade69d7
devmgardner <devm.gardner@gmail.com>
2022-08-21 19:36:59 -0400

    updated save directory

--- ytdl/dlp.py
+++ ytdl/dlp.py
@@ -7,7 +7,7 @@ sys.path.append(parentdir)
 a = [line.strip() for line in open(os.path.join('/home','ubuntu','youtube.txt')).readlines()]
 #
 for b in a:
-    subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
+    subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "/media/Dock1/Media/Videos/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
 #
 with open(os.path.join('/home','ubuntu','youtube.txt'),'w') as c:
     c.write('')
