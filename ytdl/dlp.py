88dd35e5cce062222188503be97462738519181a
devmgardner <devm.gardner@gmail.com>
2022-08-26 16:51:35 +0000

    updated script to allow for both formats of channel url

--- ytdl/dlp.py
+++ ytdl/dlp.py
@@ -11,8 +11,11 @@ now = datetime.datetime.fromtimestamp(time.time()).strftime("%m-%d-%Y_%H:%M:%S")
 for b in a:
     if not 'channel' in b:
         subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "/media/Dock1/Media/Videos/Dailies/{now}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
-    elif 'channel' in b:
-        channel = re.search('channel\/(.*)',b).group(1)
+    elif 'channel' in b or '/c/' in b:
+        try:
+            channel = re.search('channel\/(.*)',b).group(1)
+        except:
+            channel = re.search('\/c\/(.*)',b).group(1)
         subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 --download-archive {channel}.txt {b} -o "/media/Dock1/Media/Videos/Channels/{channel}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s"', shell=True)
 #
 with open(os.path.join('/home','ubuntu','youtube.txt'),'w') as c:
