d6a3c670a23d49fe744f6814359125205a2ec9ca
devmgardner <devm.gardner@gmail.com>
2022-08-26 16:40:30 +0000

    updated dlp.py to allow for archiving channels

--- ytdl/dlp.py
+++ ytdl/dlp.py
@@ -1,4 +1,4 @@
-import os,sys,subprocess,datetime,time
+import os,sys,subprocess,datetime,time,re
 #
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
@@ -9,7 +9,11 @@ a = [line.strip() for line in open(os.path.join('/home','ubuntu','youtube.txt'))
 now = datetime.datetime.fromtimestamp(time.time()).strftime("%m-%d-%Y_%H:%M:%S")
 #
 for b in a:
-    subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "/media/Dock1/Media/Videos/Dailies/{now}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
+    if not 'channel' in b:
+        subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 -o "/media/Dock1/Media/Videos/Dailies/{now}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s" {b}',shell=True)
+    elif 'channel' in b:
+        channel = re.search('channel\/(.*)',b).group(1)
+        subprocess.call(f'yt-dlp --write-info-json --embed-subs --embed-thumbnail --recode-video mp4 --download-archive {channel}.txt {b} -o "/media/Dock1/Media/Videos/Channels/{channel}/%(upload_date)s - %(title)s/%(upload_date)s - %(title)s.%(ext)s"', shell=True)
 #
 with open(os.path.join('/home','ubuntu','youtube.txt'),'w') as c:
     c.write('')
