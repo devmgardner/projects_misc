d7aa34c1c78c894967f07d5d7bd4931b101f113f
devmgardner <devm.gardner@gmail.com>
2022-09-05 17:17:20 +0000

    separating inventories by room

--- bbw_inv/inv.py
+++ bbw_inv/inv.py
@@ -4,11 +4,14 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 #
+room = input(f'enter room name> ')
+if not os.path.exists(os.path.join(currentdir,room)):
+    os.mkdir(os.path.join(currentdir,room))
 try:
     inv = {}
-    with open(os.path.join(currentdir,'dani.json'),'r') as fhand:
+    with open(os.path.join(currentdir,room,'dani.json'),'r') as fhand:
         inv['Dani'] = json.load(fhand)
-    with open(os.path.join(currentdir,'dev.json'),'r') as fhand:
+    with open(os.path.join(currentdir,room,'dev.json'),'r') as fhand:
         inv['Dev'] = json.load(fhand)
 except:
     inv = {}
@@ -19,10 +22,12 @@ running = True
 while running:
     name = input(f'scan name> ')
     if name == 'Quit':
-        with open(os.path.join(currentdir,'dani.json'),'w') as fhand:
-            fhand.write(json.dumps(inv['Dani'],indent=4))
-        with open(os.path.join(currentdir,'dev.json'),'w') as fhand:
-            fhand.write(json.dumps(inv['Dev'],indent=4))
+        if len(dani.keys()) > 0:
+            with open(os.path.join(currentdir,room,'dani.json'),'w') as fhand:
+                fhand.write(json.dumps(inv['Dani'],indent=4))
+        if len(dev.keys()) > 0:
+            with open(os.path.join(currentdir,room,'dev.json'),'w') as fhand:
+                fhand.write(json.dumps(inv['Dev'],indent=4))
         running = False
     else:
         upc = input(f'scan barcode> ')
