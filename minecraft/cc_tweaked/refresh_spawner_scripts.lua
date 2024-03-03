0e5be043a3781038c2f6808a5c8d09b8500c73ed
Devin Gardner <135624368+gardnerdevinmxd@users.noreply.github.com>
2024-03-03 11:55:20 -0500

    cleaned up refresh script

--- minecraft/cc_tweaked/refresh_spawner_scripts.lua
+++ minecraft/cc_tweaked/refresh_spawner_scripts.lua
@@ -3,9 +3,9 @@
 --- Created by devingardner.
 --- DateTime: 3/3/24 10:25 AM
 ---
-fs.delete("/spawner.lua")
-fs.delete("/start_all.lua")
-fs.delete("/end_all.lua")
-shell.run("wget https://raw.githubusercontent.com/devmgardner/projects_misc/master/minecraft/cc_tweaked/spawner.lua /spawner.lua")
-shell.run("wget https://raw.githubusercontent.com/devmgardner/projects_misc/master/minecraft/cc_tweaked/start_all.lua /start_all.lua")
-shell.run("wget https://raw.githubusercontent.com/devmgardner/projects_misc/master/minecraft/cc_tweaked/end_all.lua /end_all.lua")
+fs.delete("spawner.lua")
+fs.delete("start_all.lua")
+fs.delete("end_all.lua")
+shell.run("wget https://raw.githubusercontent.com/devmgardner/projects_misc/master/minecraft/cc_tweaked/spawner.lua spawner.lua")
+shell.run("wget https://raw.githubusercontent.com/devmgardner/projects_misc/master/minecraft/cc_tweaked/start_all.lua start_all.lua")
+shell.run("wget https://raw.githubusercontent.com/devmgardner/projects_misc/master/minecraft/cc_tweaked/end_all.lua end_all.lua")
