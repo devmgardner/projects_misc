2cb345267c2f9010881f3189f2deba3bf6a1886f
devmgardner <devm.gardner@gmail.com>
2024-04-25 19:36:54 -0400

    find.

--- minecraft/cc_tweaked/sorter/sorter.lua
+++ minecraft/cc_tweaked/sorter/sorter.lua
@@ -6,9 +6,9 @@
 
 -- need to create while loop
 -- while True, check contents of connected inventory (var name: "input_inv")
-local input_inv = peripheral.wrap("compactstorage:compact_chest")
+local input_inv = peripheral.find("compactstorage:compact_chest")
 local affix_inv = peripheral.find("storagedrawers:controller")
-local normal_inv = peripheral.wrap("storagedrawers:controller_slave")
+local normal_inv = peripheral.find("storagedrawers:controller_slave")
 -- if items in input_inv, iterate over all items
 -- if input_inv.getItemDetail(<item slot #>) has key 'lore', send to another connected inventory (var name: "affix_inv")
 -- else, send to regular connected inventory (var name: "normal_inv")
