fec590c29054298e42d12ae53e6779ae30146e1a
devmgardner <devm.gardner@gmail.com>
2024-05-07 21:57:45 -0400

    updated receiver for if less than 1k items

--- minecraft/cc_tweaked/enchant_counter/receiver.lua
+++ minecraft/cc_tweaked/enchant_counter/receiver.lua
@@ -30,11 +30,15 @@ function commas(num)
 
     local result = ""
 
-    local sign, before, after = string.match(tostring(num), "^([%+%-]?)(%d*)(%.?.*)$")
+    local before = string.match(tostring(num), "(%d*)%.?.*$")
 
-    while string.len (before) > 3 do
-        result = "," .. string.sub(before, -3, -1) .. result
-        before = string.sub(before, 1, -4)
+    if string.len(before) > 3 then
+        while string.len(before) > 3 do
+            result = "," .. string.sub(before, -3, -1) .. result
+            before = string.sub(before, 1, -4)
+        end
+    else
+        result = before
     end
 
     return result
